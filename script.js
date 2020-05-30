53401f24eeef225e11217e7f69419e3f24559d4e
Justin Suderman <jsuderman1@gmail.com>
2020-05-30 13:09:36 -0400

    added initial javascript & link tags

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 // This is my JavaScript File
+
+$.ajax({
+    url: "https://api.openbrewerydb.org/breweries",
+    method: "get",
+  }).then(function (res) {
+    console.log(res);
+  });
+
+  $.ajax({
+    url: "https://api.openbrewerydb.org/breweries?by_city=san_francisco",
+    method: "get",
+  }).then(function (res) {
+    console.log(res);
+  });
+
+  $.ajax({
+    url: "https://api.openbrewerydb.org/breweries?by_city=charlotte",
+    method: "get",
+  }).then(function (res) {
+    console.log(res);
+  });
+
+  $.ajax({
+    url: "https://api.openbrewerydb.org/breweries?by_postal=28173",
+    method: "get",
+  }).then(function (res) {
+    console.log(res);
+  });
+
+  $.ajax({
+    url: "https://api.openbrewerydb.org/breweries?by_state=north_carolina",
+    method: "get",
+  }).then(function (res) {
+    console.log(res);
+  });
+
+  $.ajax({
+    url: "https://api.openbrewerydb.org/breweries?per_page=25",
+    method: "get",
+  }).then(function (res) {
+    console.log(res);
+  });
+
+  
